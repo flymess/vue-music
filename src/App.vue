9560ab0a6de6da003d36070498fab682afd6b197
flymess <13140804565@163.com>
2017-03-03 17:59:27 +0800

    首页页面搭建完成

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,29 @@
 <template>
     <div>
-        <Masker style="border-radius: 3px;box-shadow: 3px 3px 3px #000;margin: 10px;">
-            <div class="m-img" style="background: url('https://ss0.bdstatic.com/94oJfD_bAAcT8t7mm9GUKT-xh_/timg?image&quality=100&size=b4000_4000&sec=1488462982&di=a1c48ab76d2ce980f873253b7ce2c1fc&src=http://img002.21cnimg.com/photos/album/20160326/m600/B920004B5414AE4C7D6F2BAB2966491E.jpeg') no-repeat"></div>
-            <div slot="content" class="m-title">
-                床上日用品
-                <br/>
-                <span class="m-time">2016-03-18</span>
+        <div flex="box:justify cross:center" class="header">
+            <span class="td-icon-sidebar" @click="showSidebar" :style="[showsidebar ? activeColor : '']"></span>
+            <div class="tab" flex="main:center">
+                <span class="td-icon-recommend"></span>
+                <span class="td-icon-unload"></span>
+                <span class="td-icon-find"></span>
+            </div>
+            <span class="td-icon-search"></span>
+        </div>
+
+        <Masker v-for="n in 10">
+            <div class="m-img" style="backgroundImage: url('https://cdn.xiaotaojiang.com/uploads/82/1572ec37969ee263735262dc017975/_.jpg')"></div>
+            <div slot="content" class="m-title" flex="dir:top box:last">
+                <p flex="main:left cross:bottom">点点</p>
+                <div class="content-bottom" flex="main:justify">
+                    <div flex="cross:center">
+                        <span class="td-icon-play">800</span>
+                        <span class="td-icon-add">200</span>
+                    </div>
+                    <div>
+                        <span class="td-icon-heart"></span>
+                        <span class="td-icon-bubble"></span>
+                    </div>
+                </div>
             </div>
         </Masker>
     </div>
@@ -15,19 +33,78 @@
   import {Masker} from 'vux'
 
   export default {
+    data() {
+        return {
+            activeColor: {
+                color: '#FF005A'
+            },
+            showsidebar: false
+        }
+    },
     components: {
       Masker
+    },
+    methods: {
+        showSidebar() {
+            this.showsidebar = !this.showsidebar
+        }
     }
   }
 </script>
 
 <style lang="less">
-    @import '../../vux/src/styles/reset.less';
-    body{
-        background: #F2F4F5;
+    @import '~/vux/src/styles/reset.less';
+
+    .header{
+        height:50px;
+        width: 100%;
+        font-size:24px;
+        color: #FF005A;
     }
+
+    .tab{
+        font-size: 18px;
+    }
+
+    .tab span{
+        margin:0 20px 0 20px;
+    }
+
+    .td-icon-sidebar{
+        margin-left:10px;
+        color: #dbdbdb;
+    }
+    .td-icon-sidebar:before{
+        font-family: xiang;
+        content: '\e904';
+    }
+    .td-icon-search{
+        margin-right:10px;
+        color: #dbdbdb;
+    }
+
+    .td-icon-search:before{
+        font-family: xiang;
+        content: '\e905';
+    }
+
+    .td-icon-recommend:before{
+        font-family: xiang;
+        content: '\e906';
+    }
+
+    .td-icon-unload:before{
+        font-family: xiang;
+        content: '\e907';
+    }
+
+    .td-icon-find:before{
+        font-family: xiang;
+        content: '\e908';
+    }
+
     .m-img {
-        padding-bottom: 33%;
+        padding-bottom: 40%;
         display: block;
         position: relative;
         max-width: 100%;
@@ -36,25 +113,76 @@
         cursor: pointer;
         border-radius: 2px;
     }
-    .m-title {
+    .m-title{
+        width:100%;
+        height:100%;
         color: #fff;
-        text-align: center;
-        text-shadow: 0 0 2px rgba(0, 0, 0, .5);
-        font-weight: 500;
-        font-size: 16px;
-        position: absolute;
-        left: 0;
-        right: 0;
-        width: 100%;
-        text-align: center;
-        top: 50%;
-        transform: translateY(-50%);
     }
-    .m-time {
-        font-size: 12px;
-        padding-top: 4px;
-        border-top: 1px solid #f0f0f0;
-        display: inline-block;
-        margin-top: 5px;
+    .m-title p{
+        text-align: center;
+        font-size:18px;
+        padding-left:20px;
+    }
+    .content-bottom{
+        width:100%;
+        height:30px;
+        font-size:12px;
+        padding-left:16px;
+    }
+    .content-bottom img{
+        width:30px;
+        height:30px;
+        border-radius: 50%;
+    }
+    .content-bottom span{
+        font-size:16px;
+    }
+    .content-bottom>div:first-child img{
+        margin-right:10px;
+    }
+
+    .content-bottom .td-icon-play{
+        margin-right: 5px;
+        position: relative;
+        top:-2px;
+    }
+
+    .content-bottom .td-icon-play:before{
+        font-family: xiang;
+        content: '\e901';
+        margin-right:5px;
+        font-size:22px;
+        position: relative;
+        top:3px;
+    }
+
+    .content-bottom .td-icon-add:before{
+        font-family: xiang;
+        content: '\e903';
+        margin-right:5px;
+        font-size:18px;
+        position: relative;
+        top:1px;
+    }
+
+    .content-bottom .td-icon-heart{
+        font-size:24px;
+        margin-right:5px;
+        color: #af1d4e;
+        opacity: .8;
+    }
+    .content-bottom .td-icon-bubble{
+        font-size:24px;
+        margin-right:5px;
+        color: #e6e6e6;
+        opacity: .8;
+    }
+    .td-icon-heart:before{
+        font-family: xiang;
+        content: '\e900';
+    }
+    .td-icon-bubble:before{
+        font-family: xiang;
+        content: '\e902';
     }
 </style>
